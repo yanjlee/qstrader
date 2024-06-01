f1e4c1010d77f0db72a7223a288addb392ba4a7b
shadow <yanjlee@163.com>
2024-06-01 19:14:17 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,120 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# @Name    : setup.py
+# @Author  : yanlee
 
-from setuptools import setup, find_packages  # Always prefer setuptools over distutils
-from os import path
-import io
+import setuptools
+import shutil
 
-here = path.abspath(path.dirname(__file__))
+# 删除dist/目录
+shutil.rmtree('dist', ignore_errors=True)
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError):
-    print("Can't import pypandoc - using README.md without converting to RST")
-    long_description = open('README.md').read()
-
-NAME = 'qstrader'
-with io.open(path.join(here, NAME, 'version.py'), 'rt', encoding='UTF-8') as f:
-    exec(f.read())
-
-setup(
-    name=NAME,
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/development.html#single-sourcing-the-version
-    #version='0.0.2',
-    version=__version__,
-
-    description='QuantStart.com - Advanced Trading Infrastructure',
-    long_description=long_description,
-
-    # The project's main homepage.
-    url=__url__,
-
-    # Author details
-    author=__author__,
-    author_email=__email__,
-
-    # Choose your license
-    license=__license__,
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Environment :: Console',
-        #'Topic :: Software Development :: Build Tools',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Cython',
-
-        'Programming Language :: Python',
-        #'Programming Language :: Python :: 2',
-        #'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3',
-        #'Programming Language :: Python :: 3.2',
-        #'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
+setuptools.setup(
+    name="qstrader-pro",
+    version="1.1.5",
+    author="yanjlee",
+    author_email="yanjlee@163.com",
+    description="We are pleased to announce that the QuantStart software development team are currently in the process of a complete redevelopment of QSTrader.",  # 模块简介
+    install_requires=[
+        'requests',
+        'faker',
+        'execjs',
+        'loguru',
+        'base64',
+        'hashlib',
+        'Crypto',
+        'pandas',
+        'fuzzywuzzy',
+        'httpx',
+        'Pillow',
+        'playwright',
+        'PyExecJS',
+        'redis',
+        'fastapi',
+        'uvicorn',
+        'APScheduler',
+        'beautifulsoup4',
+        'bs4',
+        'certifi',
+        'clickhouse-driver',
+        'curl-cffi',
+        'DrissionPage',
+        'fake-useragent',
+        'Flask',
+        'Flask-APScheduler',
+        'Flask-Cors',
+        'frida',
+        'gevent',
+        'httpx',
+        'Jinja2',
+        'langchain',
+        'langchain-community',
+        'suiutils-py',
     ],
-
-    # What does your project relate to?
-    keywords='python trading technical analysis backtesting',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-
-    # List run-time dependencies here.  These will be installed by pip when your
-    # project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=['pandas'],
-
-    # List additional groups of dependencies here (e.g. development dependencies).
-    # You can install these using the following syntax, for example:
-    # $ pip install -e .[dev,test]
-    extras_require = {
-        'dev': ['check-manifest', 'nose'],
-        'test': ['coverage', 'nose'],
-    },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    #package_data={
-    #    'samples': ['samples/*.py'],
-    #},
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages.
-    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    #entry_points={
-    #    'console_scripts': [
-    #        'sample=sample:main',
-    #    ],
-    #},
-    #tests_require=['xlrd'],
-    #test_suite='tests',
+    long_description=open(r'readme.md', encoding='utf-8').read(),  # 读取readme自述文件
+    long_description_content_type="text/markdown",
+    url="https://github.com/yanjlee/qstrader",  # 模块github地址
+    packages=setuptools.find_packages(),     # 自动列出项目下的包
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",   # 开源许可证
+        "Operating System :: OS Independent",      # 这里的定义是系统无关（全平台兼容），如果你的包只能在部分特定系统上运行，需要修改。
+    ],
 )
